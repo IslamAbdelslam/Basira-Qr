2e7575ee729453f988a24168716e80956198a19c
IslamAbdelslam <islamabdoulradi@gmail.com>
2025-10-09 01:17:58 +0300

    refactoring our frontend

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,26 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
+
 const config = getDefaultConfig(__dirname);
+
+// Optimize for smaller bundle size
+config.transformer.minifierConfig = {
+  keep_fnames: true,
+  mangle: {
+    keep_fnames: true,
+  },
+};
+
+// Enable tree shaking
+config.resolver.platforms = ["native", "android", "ios", "web"];
+
+// Exclude unnecessary files from bundle
+config.resolver.blockList = [
+  /docs\/.*/,
+  /landing-page\/.*/,
+  /\.git\/.*/,
+  /node_modules\/.*\/README\.md/,
+  /node_modules\/.*\/CHANGELOG\.md/,
+  /node_modules\/.*\/LICENSE/,
+];
+
 module.exports = config;
